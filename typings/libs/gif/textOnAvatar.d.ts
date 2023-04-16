a4bc0c6acb5ebade3792686e65d53cc24fbc8a64
hisRoyalty <hisroyal.baguette@gmail.com>
2023-04-16 19:59:47 +0800

    :label: update typings for textOnAvatar

--- typings/libs/gif/textOnAvatar.d.ts
+++ typings/libs/gif/textOnAvatar.d.ts
@@ -1,2 +1,2 @@
-declare function _exports(image: any, args: any): Promise<any>;
+declare function _exports(image: any, args: any, delay?: number): Promise<any>;
 export = _exports;
--- typings/src/Main.d.ts
+++ typings/src/Main.d.ts
@@ -22,9 +22,10 @@ declare class Main {
      * Puts text on a GIF, likewise a 'meme'.
      * @param {Buffer|string} image Image to use for the manipulation.
      * @param {string} args Text which is displayed on the returned GIF. It is separated by spaces.
+     * @param {number} delay Delay between frames.
      * @returns {Promise<Buffer>}
      */
-    static textOnAvatar(image: Buffer | string, args: string): Promise<Buffer>;
+    static textOnAvatar(image: Buffer | string, args: string, delay?: number): Promise<Buffer>;
     /**
      * Applies a rain effect to the image. Returns a GIF.
      * @param {Buffer|string} image Image to use for the manipulation.
